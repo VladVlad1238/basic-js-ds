c51bddd0a2f6c80cc6c6bae251467fb879077ae0
Vladyslav Sikalo <vladislavmediol@gmail.com>
2025-01-04 10:44:38 +0100

    feat: stack done

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement the Stack with a given interface via array.
@@ -13,22 +13,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.stack = [];
   }
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  push(element) {
+    this.stack.push(element);
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.stack.length === 0) {
+      return undefined;
+    }
+
+    return this.stack[this.stack.length - 1];
+  }
+
+  pop() {
+    if (this.stack.length === 0) {
+      return undefined;
+    }
+
+    return this.stack.pop();
   }
 }
 
 module.exports = {
-  Stack
+  Stack,
 };
